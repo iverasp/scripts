5ae9b072167dedcc6221568aa9d2f98c22603c1d
Iver Egge <iverasp@gmail.com>
2016-05-14 15:38:58 +0200

    now with ipv6

--- ipupdate.sh
+++ ipupdate.sh
@@ -3,40 +3,73 @@
 # Needs dnsutils in debian
 
 # Generate key with
-# ddns-confgen -r /dev/urandom -q -a hmac-md5 -k home.iegget.no -s home.iegget.no. | tee -a /etc/bind/keys/home.iegget.no.keys > /etc/bind/keys/key.home.iegget.no
+# dnssec-keygen -a HMAC-MD5 -b 512 -n HOST <name>
 
 # set some variables
 zone=home.iegget.no
-dnsserver=ns1.iegget.no
-#keyfile=/home/iver/scripts/key.lek.iegget.no
+dnsserver=129.241.105.205
 keyfile=home.key
-#
- 
+ipv6_enabled=true
+initial_run=false
+
 # get current external address
-ext_ip=`dig +short @resolver1.opendns.com myip.opendns.com`
+ext_ipv4=`dig +short A @resolver1.opendns.com myip.opendns.com`
+ext_ipv6=`curl --silent https://ipv6.icanhazip.com/`
  
 # get last ip address from the DNS server
-last_ip=`dig +short @$dnsserver $zone`
+last_ipv4=`dig +short A @$dnsserver $zone`
+last_ipv6=`dig +short AAAA @$dnsserver $zone`
+if [ "$initial_run" = true ]; then
+    last_ipv4="none"
+    last_ipv6="none"
+fi
+
+if [ ! -z "$ext_ipv4" ]; then
+    if [ ! -z "$last_ipv4" ]; then
+        if [ "$ext_ipv4" != "$last_ipv4" ]; then
+            echo "IPv4 addresses do not match (external=$ext_ipv4, last=$last_ipv4), sending an update"
  
-if [ ! -z "$ext_ip" ]; then
-   if [ ! -z "$last_ip" ]; then
-      if [ "$ext_ip" != "$last_ip" ]; then
-         echo "IP addresses do not match (external=$ext_ip, last=$last_ip), sending an update"
- 
-         echo "server $dnsserver
-         zone $zone.
-         update delete $zone.
-         update add $zone. 60 A $ext_ip
-         update add $zone. 60 TXT 'Updated on $(date)'
-         send" > /tmp/ipupdate.tmp
-         nsupdate -k $keyfile -v /tmp/ipupdate.tmp
+            echo "server $dnsserver
+            zone $zone.
+            update delete $zone. A
+            update delete $zone. TXT
+            update add $zone. 60 A $ext_ipv4 
+            update add $zone. 60 TXT 'Updated on $(date)'
+            send" > /tmp/ipupdate.tmp
+            nsupdate -k $keyfile -v /tmp/ipupdate.tmp
           
-      else
-         echo "success: IP addresses match (external=$ext_ip, last=$last_ip), nothing to do"
-      fi
-   else
-      echo "fail: couldn't resolve last ip address from $dnsserver"
-   fi
+        else
+            echo "success: IPv4 addresses match (external=$ext_ipv4, last=$last_ipv4), nothing to do"
+        fi
+    else
+        echo "fail: couldn't resolve last IPv4 address from $dnsserver"
+    fi
 else
-   echo "fail: couldn't resolve current external ip address from resolver1.opendns.com"
+    echo "fail: couldn't resolve current external IPv4 address from resolver1.opendns.com"
+fi
+
+if [ "$ipv6_enabled" = true ]; then
+    if [ ! -z "$ext_ipv6" ]; then
+        if [ ! -z "$last_ipv6" ]; then
+            if [ "$ext_ipv6" != "$last_ipv6" ]; then
+                echo "IPv6 addresses do not match (external=$ext_ipv6, last=$last_ipv6), sending an update"
+ 
+                echo "server $dnsserver
+                zone $zone.
+                update delete $zone. AAAA
+                update delete $zone. TXT
+                update add $zone. 60 AAAA $ext_ipv6
+                update add $zone. 60 TXT 'Updated on $(date)'
+                send" > /tmp/ipupdate.tmp
+                nsupdate -k $keyfile -v /tmp/ipupdate.tmp
+          
+            else
+                echo "success: IPv6 addresses match (external=$ext_ipv6, last=$last_ipv6), nothing to do"
+            fi
+        else
+            echo "fail: couldn't resolve last IPv6 address from $dnsserver"
+        fi
+    else
+        echo "fail: couldn't resolve current external IPv6 address from ipv6.icanhazip.com"
+    fi
 fi
